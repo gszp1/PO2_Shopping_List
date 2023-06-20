419dbc889501f44fc4cbcdc4cfa5e182b1084136
gszp1 <grzybekmateusz@poczta.fm>
2023-06-20 18:24:54 +0200

    Split allowed types

--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,20 @@
 public class Product {
-    public static String [] allowedTypes = {"kg", "l", "sztuk/a", "metr"};
+    public static String [] allowedTypes;
+
+    public static String [] integerTypes = {"sztuk/a"};
+
+    public static String [] floatTypes = {"kg", "l", "metr"};
+
+    static {
+        allowedTypes = new String[integerTypes.length + floatTypes.length];
+        int i = 0;
+        for (String j : integerTypes) {
+            allowedTypes[i++] = j;
+        }
+        for (String j : floatTypes) {
+            allowedTypes[i++] = j;
+        }
+    }
 
     private final String name;
 
@@ -26,10 +41,21 @@ public class Product {
     }
 
     public String typeParser() {
-        if(("kg".compareTo(type) == 0) || ("l".compareTo(type) == 0)) {
+        if(isFloatType(type)) {
             return Float.toString(quantity);
         }
         return Integer.toString((int)quantity);
     }
+
+    private boolean isFloatType(String type) {
+        for(String i: floatTypes) {
+            if (i.equals(type)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 
 }
